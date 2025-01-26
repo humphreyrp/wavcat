b59d06c9cf7101eb3bd6f59aa35c581aed4786ac
Robert Humphrey <humphreyrp92@gmail.com>
2025-01-26 15:49:01 -0500

    Add initial arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+
 fn main() {
-    println!("Hello, world!");
+    let mut verbose = false;
+    let mut path = "".to_string();
+
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("display a spectrogram view of the provided .wav file");
+        ap.refer(&mut path).add_option(&["-f", "--filepath"], Store, "File to display").required();
+        ap.refer(&mut verbose).add_option(&["-v","--verbose"], StoreTrue, "Be verbose");
+        ap.parse_args_or_exit();
+    }
+
+    println!("Displaying wav file: {}", path);
 }
