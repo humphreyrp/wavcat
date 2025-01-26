fbb8320ce6bc1e13f9d569d648e06797228ce1ec
Robert Humphrey <humphreyrp92@gmail.com>
2025-01-26 16:13:45 -0500

    Verify path exists

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 extern crate argparse;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
+use std::path::Path;
+use std::process;
 
 fn main() {
     let mut verbose = false;
-    let mut path = "".to_string();
+    let mut fp = "".to_string();
 
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("display a spectrogram view of the provided .wav file");
-        ap.refer(&mut path).add_option(&["-f", "--filepath"], Store, "File to display").required();
+        ap.refer(&mut fp).add_option(&["-f", "--filepath"], Store, "File to display").required();
         ap.refer(&mut verbose).add_option(&["-v","--verbose"], StoreTrue, "Be verbose");
         ap.parse_args_or_exit();
     }
 
-    println!("Displaying wav file: {}", path);
+    // Verify path exists
+    if !Path::new(&fp).exists()
+    {
+        println!("File does not exist");
+        process::exit(1);
+    }
+
+    println!("Displaying wav file: {}", fp);
 }
