9f91172d27a40c908b2c0ee885f4b6c4fe0b612e
Robert Humphrey <humphreyrp92@gmail.com>
2025-01-28 09:40:11 -0500

    WIP, add some more dependencies for printing images

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate argparse;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
+use wavers::Wav;
 use std::path::Path;
 use std::process;
+use viuer::{print, Config};
+use std::{thread, time};
 
 fn main() {
     let mut verbose = false;
-    let mut fp = "".to_string();
+    let mut fp: String = "".to_string();
 
     {
         let mut ap = ArgumentParser::new();
@@ -23,5 +26,19 @@ fn main() {
         process::exit(1);
     }
 
-    println!("Displaying wav file: {}", fp);
+    // Load wav file
+    let _wav: Wav<i16> = Wav::from_path(&fp).unwrap();
+
+    // Start adding image framework
+    let conf = Config {
+        // Start from row 4 and column 20.
+        x: 20,
+        y: 4,
+        ..Default::default()
+    };
+
+    let img = image::DynamicImage::ImageRgba8(image::RgbaImage::new(20, 10));
+    print(&img, &conf).expect("Image printing failed.");
+
+    thread::sleep(time::Duration::from_millis(1000));
 }
