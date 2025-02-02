dccb545cf50968a0577581a84b0915c7db067be9
Robert Humphrey <humphreyrp92@gmail.com>
2025-02-02 13:07:02 -0500

    Run formatter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate argparse;
 
-use argparse::{ArgumentParser, StoreTrue, Store};
-use wavers::{Wav};
+use argparse::{ArgumentParser, Store, StoreTrue};
+use image::{DynamicImage, Pixel, Rgba, RgbaImage};
 use std::path::Path;
 use std::process;
 use viuer;
-use image::{DynamicImage, Pixel, Rgba, RgbaImage};
+use wavers::Wav;
 
 fn main() {
     let mut verbose = false;
@@ -14,14 +14,16 @@ fn main() {
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("display a spectrogram view of the provided .wav file");
-        ap.refer(&mut fp).add_option(&["-f", "--filepath"], Store, "File to display").required();
-        ap.refer(&mut verbose).add_option(&["-v","--verbose"], StoreTrue, "Be verbose");
+        ap.refer(&mut fp)
+            .add_option(&["-f", "--filepath"], Store, "File to display")
+            .required();
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
         ap.parse_args_or_exit();
     }
 
     // Verify path exists
-    if !Path::new(&fp).exists()
-    {
+    if !Path::new(&fp).exists() {
         println!("File does not exist");
         process::exit(1);
     }
@@ -29,8 +31,7 @@ fn main() {
     // Loop through the file in chunks
     let chunk_size = 16;
     let mut wav: Wav<i16> = Wav::from_path(&fp).unwrap();
-    let _res = loop
-    {
+    let _res = loop {
         // let _res = wav.read_samples(16);
         let res2 = wav.seek_by_samples(chunk_size);
         let _data = match res2 {
